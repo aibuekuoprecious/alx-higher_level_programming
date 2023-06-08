bdd769e69c4a784b1f346e1906b056a999da2e66
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-06-08 16:29:10 -0700

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+args_count = len(sys.argv) - 1
+if args_count == 0:
+    print("0 arguments.")
+elif args_count == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(args_count))
+for i in range(args_count):
+     print("{}: {}".format(i + 1, sys.argv[i + 1]))

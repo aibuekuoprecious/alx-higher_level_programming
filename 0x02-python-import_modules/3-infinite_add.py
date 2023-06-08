27a58d302e51e82dfaf14ee423d08f9464316e33
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-06-08 16:44:58 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
-add = 0
-for arg in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+result = 0
+for s in argv[1:]:
+    result += int(s)
+print("{:d}".format(result))

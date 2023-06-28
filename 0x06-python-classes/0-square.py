77ea7bde34eb12bd2408c829bf9b34e38205739e
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-06-27 21:53:24 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class Square:
+    """ Declares a square class """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """ Declares a square class """
+
+    def __init__(self, size) -> None:
+        """
+        Initializes class attributes
+        Args:
+            size: size of square
+        """
+        self.__size = size
